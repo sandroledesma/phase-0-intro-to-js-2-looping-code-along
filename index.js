d62d900d78a3cc3f2d7725ff333c1a2188a9e0ae
Alessandro <sandroledesma@gmail.com>
2024-02-29 21:34:20 -0500

    add files

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    const thankyouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankyouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankyouMessages;
+}
+
+function countDown(startingnumber) {    
+    while (startingnumber >= 0) {
+        console.log(startingnumber);
+        startingnumber--;
+    }
+}
